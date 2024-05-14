4a0bbfb8b7f04bbf27a5f5ccaf137ae61bd8485c
Simon Frydrych <simon.frydrych@ausy-technologies.de>
2024-05-14 14:02:17 +0200

    fix permitAll

--- src/main/java/com/randstaddigital/workshop/security/AuthTokenSecurityConfig.java
+++ src/main/java/com/randstaddigital/workshop/security/AuthTokenSecurityConfig.java
@@ -1,12 +1,13 @@
 package com.randstaddigital.workshop.security;
 
+import static jakarta.servlet.DispatcherType.ERROR;
+import static jakarta.servlet.DispatcherType.FORWARD;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 import java.util.Objects;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,9 +16,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @Configuration
 @EnableWebSecurity
@@ -36,27 +34,21 @@ public class AuthTokenSecurityConfig {
   }
 
   @Bean
-  public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc)
-      throws Exception {
-    return http.securityMatcher("/**")
-        .csrf(AbstractHttpConfigurer::disable)
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.csrf(AbstractHttpConfigurer::disable)
         .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable))
         .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
-        .addFilterBefore(createApiKeyFilter(), UsernamePasswordAuthenticationFilter.class)
+        .addFilter(createApiKeyFilter())
         .authorizeHttpRequests(
-            authorizeHttpRequests ->
-                authorizeHttpRequests
-                    .requestMatchers(mvc.pattern("/actuator/*"))
+            requests ->
+                requests
+                    .dispatcherTypeMatchers(FORWARD, ERROR)
+                    .permitAll()
+                    .requestMatchers("/bike-rental/v1/actuator", "/bike-rental/v1/actuator/*")
                     .permitAll()
                     .anyRequest()
                     .authenticated())
         .build();
-  }
-
-  @Scope("prototype")
-  @Bean
-  MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
-    return new MvcRequestMatcher.Builder(introspector);
   }
 
   private AuthTokenFilter createApiKeyFilter() {
